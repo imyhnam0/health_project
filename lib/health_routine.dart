772fd23443a41aad1f274f3a3009ec23baa5a846
JW-Ahn0 <jun.woo.an0@gmail.com>
2023-12-28 00:45:15 +0900

    헬스 루틴 23.12.27

--- lib/health_routine.dart
+++ lib/health_routine.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 
-class HealthRoutineMenu extends StatelessWidget{
-  const HealthRoutineMenu({super.key});
+class CreateRoutine extends StatelessWidget {
+  const CreateRoutine({super.key});
 
   @override
+
+
   Widget build(BuildContext context) {
     return Scaffold(
-      // UI의 해당 부분이 미완성임을 표시하는 위젯
-      body: const Placeholder(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          // TextField 추가
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Enter your routine name : ',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'name:  ',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ],
+
+
+      ),
 
       // main page로 돌아가는 버튼
       // navigator 사용법은 다음 웹페이지에서 설명함
-      // https://docs.flutter.dev/cookbook/navigation/navigation-basics
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {Navigator.pop(context);},
         tooltip: 'Home menu',
@@ -20,3 +42,86 @@ class HealthRoutineMenu extends StatelessWidget{
     );
   }
 }
+
+class HealthRoutineMenu extends StatelessWidget{
+  const HealthRoutineMenu({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            // 헬스 루틴 메뉴
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                fixedSize: const Size(300, 100),
+              ),
+              child: const Text('루틴생성',
+                style: TextStyle(fontSize: 18),
+              ),
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const CreateRoutine()),
+                  );
+                }
+
+
+
+            ),
+            const SizedBox(height: 80),
+
+            //식단 메뉴
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                fixedSize: const Size(300, 100),
+              ),
+              child: const Text('즐겨찾기',
+                style: TextStyle(fontSize: 18),
+              ),
+                onPressed: (){}
+
+            ),
+            const SizedBox(height: 80),
+
+            //친구 추가 메뉴
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                fixedSize: const Size(300, 100),
+              ),
+              child: const Text('달력',
+                style: TextStyle(fontSize: 18),
+              ),
+                onPressed: (){}
+
+            ),
+          ],
+        ),
+      ),
+
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {Navigator.pop(context);},
+        tooltip: 'Home menu',
+        child: const Icon(Icons.home),
+      ),
+    );
+  }
+}
+
