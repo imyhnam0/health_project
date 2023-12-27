d24c31dc641d25b031cfa3edfe7b560ab0b6fbf1
jinnkim0112 <jinnkim0112@gmail.com>
2023-12-27 17:42:13 +0900

    따로 개발하기 편하게  파일 분리

---
+++ lib/friends.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FriendsMenu extends StatelessWidget{
+  const FriendsMenu({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // UI의 해당 부분이 미완성임을 표시하는 위젯
+      body: Placeholder(),
+
+      // main page로 돌아가는 버튼
+      // navigator 사용법은 다음 웹페이지에서 설명함
+      // https://docs.flutter.dev/cookbook/navigation/navigation-basics
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {Navigator.pop(context);},
+        tooltip: 'Home menu',
+        child: const Icon(Icons.home),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_project/health_routine.dart';
+import 'package:flutter_project/diet.dart';
+import 'package:flutter_project/friends.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,137 +10,96 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Health',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Health routine&food'),
+      home: const MyHomePage(),
     );
   }
 }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-
-    });
-  }
+  final title = 'Health routine&food';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(0),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            // 헬스 루틴 메뉴
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                fixedSize: const Size(300, 100),
+              ),
+              child: const Text('헬스 루틴',
+                style: TextStyle(fontSize: 18),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HealthRoutineMenu()),
+                );
+              },
             ),
-            fixedSize: const Size(300, 100),
-          ),
+            const SizedBox(height: 80),
 
-        onPressed: () {},
-          child: const Text('헬스 루틴',
-            style: TextStyle(fontSize: 18),
-          ),
-      ),
-              const SizedBox(height: 80),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0),
-                  ),
-                  fixedSize: const Size(300, 100),
+            //식단 메뉴
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
                 ),
-                child: const Text('식단',
-                  style: TextStyle(fontSize: 18),
-                ),
-        onPressed: () {},
-      ),
-              const SizedBox(height: 80),
-              ElevatedButton(
+                fixedSize: const Size(300, 100),
+              ),
+              child: const Text('식단',
+                style: TextStyle(fontSize: 18),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const DietMenu()),
+                );
+              },
+            ),
+            const SizedBox(height: 80),
 
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0),
-                  ),
-                  fixedSize: const Size(300, 100),
+            //친구 추가 메뉴
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
                 ),
-                child: const Text('친구 추가',
-                  style: TextStyle(fontSize: 18),
-                ),
-
-        onPressed: () {},
-      ),
-      ],
+                fixedSize: const Size(300, 100),
+              ),
+              child: const Text('친구 추가',
+                style: TextStyle(fontSize: 18),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FriendsMenu()),
+                );
+              },
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
