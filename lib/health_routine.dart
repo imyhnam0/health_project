34cdfb6c7fcde947002faed0755e3b9c8281a5bf
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-01-02 13:54:42 +0900

    헬스 루틴 24.01.02

--- lib/health_routine.dart
+++ lib/health_routine.dart
@@ -1,48 +1,184 @@
 import 'package:flutter/material.dart';
 
-class CreateRoutine extends StatelessWidget {
-  const CreateRoutine({super.key});
+class CreateRoutine extends StatefulWidget {
+  const CreateRoutine({Key? key}) : super(key: key);
+
+  @override
+  State<CreateRoutine> createState() => _CreateRoutineState();
+}
+
+class _CreateRoutineState extends State<CreateRoutine> {
+
+
+  List<Widget> textFieldRows = [
+    // Initial row of text fields
+    Row(
+      children: [
+        Expanded(
+          child: Container(
+            width: 100,
+            height: 100,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Name: ',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            width: 100,
+            height: 100,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Sets: ',
+                border: OutlineInputBorder(),
+              ),
+
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            width: 100,
+            height: 100,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'kg: ',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            width: 100,
+            height: 100,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'reps: ',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  ];
+
 
   @override
 
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
-          // TextField 추가
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Enter your routine name :',
-              border: OutlineInputBorder(),
-            ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Name: ',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              const SizedBox(height: 10),
+              // Display the list of text field rows
+              Column(children: textFieldRows),
+              const SizedBox(height: 10),
+              _elevatedButton(context),
+
+            ],
           ),
-          const SizedBox(height: 20),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'name:  ',
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ],
-
-
+        ),
       ),
-
-      // main page로 돌아가는 버튼
-      // navigator 사용법은 다음 웹페이지에서 설명함
-
       floatingActionButton: FloatingActionButton(
-        onPressed: () {Navigator.pop(context);},
+        onPressed: () {
+          Navigator.pop(context);
+        },
         tooltip: 'Home menu',
         child: const Icon(Icons.home),
       ),
     );
   }
-}
 
+  Widget _elevatedButton(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0),
+        ),
+        fixedSize: const Size(10, 10),
+      ),
+      child: const Text(
+        '+',
+        style: TextStyle(fontSize: 18),
+      ),
+      onPressed: () {
+        // Add a new row of text fields when the '+' button is pressed
+        setState(() {
+          textFieldRows.add(
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Name: ',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Sets: ',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'kg: ',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'reps: ',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+      },
+    );
+  }
+}
 class HealthRoutineMenu extends StatelessWidget{
   const HealthRoutineMenu({super.key});
 
@@ -59,15 +195,15 @@ class HealthRoutineMenu extends StatelessWidget{
 
             // 헬스 루틴 메뉴
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(0),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  fixedSize: const Size(300, 100),
                 ),
-                fixedSize: const Size(300, 100),
-              ),
-              child: const Text('루틴생성',
-                style: TextStyle(fontSize: 18),
-              ),
+                child: const Text('루틴생성',
+                  style: TextStyle(fontSize: 18),
+                ),
                 onPressed: (){
                   Navigator.push(
                     context,
@@ -82,15 +218,15 @@ class HealthRoutineMenu extends StatelessWidget{
 
             //식단 메뉴
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(0),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  fixedSize: const Size(300, 100),
                 ),
-                fixedSize: const Size(300, 100),
-              ),
-              child: const Text('즐겨찾기',
-                style: TextStyle(fontSize: 18),
-              ),
+                child: const Text('즐겨찾기',
+                  style: TextStyle(fontSize: 18),
+                ),
                 onPressed: (){}
 
             ),
@@ -98,15 +234,15 @@ class HealthRoutineMenu extends StatelessWidget{
 
             //친구 추가 메뉴
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(0),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  fixedSize: const Size(300, 100),
                 ),
-                fixedSize: const Size(300, 100),
-              ),
-              child: const Text('달력',
-                style: TextStyle(fontSize: 18),
-              ),
+                child: const Text('달력',
+                  style: TextStyle(fontSize: 18),
+                ),
                 onPressed: (){}
 
             ),
