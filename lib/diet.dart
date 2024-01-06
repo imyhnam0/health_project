7bf11334f15ce74e6fbf7c52545c5f25b9cec030
jinnkim0112 <jinnkim0112@gmail.com>
2024-01-06 15:26:27 +0900

    added calendar

--- lib/diet.dart
+++ lib/diet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_project/today.dart';
 
 class DietMenu extends StatefulWidget{
@@ -11,7 +10,17 @@ class DietMenu extends StatefulWidget{
 }
 
 class _DietMenuState extends State<DietMenu> {
-  TodayDiet diet = TodayDiet();
+  Diet diet = Diet();
+
+  @override
+  void initState() {
+    super.initState();
+    _initiate();
+  }
+
+  Future<void> _initiate() async {
+    //dietHistory = Diet.json(jsonDecode(await file.readAsString()));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +29,13 @@ class _DietMenuState extends State<DietMenu> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('diet'),
+        leading: BackButton(
+                  onPressed: () {
+                    setState(() {
+                      Navigator.pop(context);
+                    });
+                  },
+                )
       ),
       body: Center(
         child: Column(
@@ -34,12 +50,10 @@ class _DietMenuState extends State<DietMenu> {
               ),
               child: const Text('오늘 식단', style: TextStyle(fontSize: 18)),
               onPressed: () {
-                setState(() async {
-                  diet = await Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TodayMenu(diet: diet))
-                  );
-                });
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TodayMenu(diet: diet))
+                );
               },
             ),
             const SizedBox(height: 80),
@@ -52,12 +66,26 @@ class _DietMenuState extends State<DietMenu> {
               ),
               child: const Text('즐겨찾기', style: TextStyle(fontSize: 18)),
               onPressed: () {
-                setState(() async {
-                  await Navigator.push(
+                Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => History(diet: diet))
                   );
-                });
+              },
+            ),
+            const SizedBox(height: 80),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                fixedSize: const Size(300, 100),
+              ),
+              child: const Text('달력', style: TextStyle(fontSize: 18)),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Calandar(diet: diet))
+                  );
               },
             ),
           ],
@@ -68,8 +96,10 @@ class _DietMenuState extends State<DietMenu> {
 }
 
 class TodayMenu extends StatefulWidget {
-  final TodayDiet diet;
-  const TodayMenu({super.key, required this.diet});
+  final Diet diet;
+  final String? name;
+  final int? gram, kcal;
+  const TodayMenu({super.key, required this.diet, this.name, this.gram, this.kcal});
 
   @override
   State<TodayMenu> createState() => _TodayMenuState();
@@ -81,6 +111,22 @@ class _TodayMenuState extends State<TodayMenu> {
   final nameCtrl = TextEditingController();
   final gramCtrl = TextEditingController();
   final kcalCtrl = TextEditingController();
+  
+  @override
+  void initState() {
+    super.initState();
+    nameCtrl.text = widget.name ?? '';
+    gramCtrl.text = widget.gram?.toString() ?? '';
+    kcalCtrl.text = widget.kcal?.toString() ?? '';
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameCtrl.dispose();
+    gramCtrl.dispose();
+    kcalCtrl.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -90,13 +136,7 @@ class _TodayMenuState extends State<TodayMenu> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Today Diet'),
-        leading: BackButton(
-                  onPressed: () {
-                    setState(() {
-                      Navigator.pop(context, diet);
-                    });
-                  },
-                )
+        leading: BackButton(onPressed: () {Navigator.pop(context);},)
       ),
       body: Center(
         child: Column(
@@ -147,7 +187,7 @@ class _TodayMenuState extends State<TodayMenu> {
                     builder: (context) => AlertDialog(
                       content: const Text('name must be not empty'),
                       actions: <Widget>[
-                        TextButton(
+                        TextButton( 
                           child: const Text('Cancel'),
                           onPressed: () => Navigator.pop(context),
                         ),
@@ -185,10 +225,21 @@ class _TodayMenuState extends State<TodayMenu> {
                   )
                 );
               },
-              child: const Text('Add'),
+              child: const Icon(Icons.add),
             ),
           ],
         )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => History(diet: diet))
+          );
+        },
+        tooltip: 'Home menu',
+        child: const Icon(Icons.bookmark),
       ),
     );
   }
@@ -197,9 +248,10 @@ class _TodayMenuState extends State<TodayMenu> {
 // 다음 페이지의 sample code에 따라 수정할 것
 //https://api.flutter.dev/flutter/widgets/ListView-class.html
 class History extends StatefulWidget {
-  final TodayDiet diet;
+  final Diet diet;
+  final DateTimeRange? dateRange;
 
-  const History({super.key, required this.diet});
+  const History({super.key, required this.diet, this.dateRange});
 
   @override
   State<History> createState() => _HistoryState();
@@ -207,15 +259,36 @@ class History extends StatefulWidget {
 
 class _HistoryState extends State<History> {
   bool isSelectionMode = false;
+  bool? totalCheck;
   late List<bool> _selected;
+  late Diet _selectedDiet;
 
   _HistoryState();
 
   @override
+  void initState() {
+    super.initState();
+    _selectedDiet = widget.dateRange == null ? widget.diet : widget.diet.dietOnDateRange(widget.dateRange!);
+    setSelection(false, _selectedDiet);
+  }
+
+  void setSelection(bool selected, Diet diet) {
+    _selected = List<bool>.generate(diet.meals.length, (_) => selected);
+  }
+
+  void _toggle(int index) {
+    setState(() {
+      _selected[index] = !_selected[index];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    TodayDiet diet = widget.diet;
-    _selected = List<bool>.generate(diet.meals.length, (_) => false);
-    return Scaffold(  
+    if (_selected.isNotEmpty) totalCheck = null;
+    for (var element in _selected) {
+      if(element != totalCheck) totalCheck = totalCheck == null ? element : null;
+    }
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('history'),
@@ -225,6 +298,7 @@ class _HistoryState extends State<History> {
                     onPressed: () {
                       setState(() {
                         isSelectionMode = false;
+                        setSelection(false, _selectedDiet);
                       });
                     },
                   )
@@ -234,13 +308,49 @@ class _HistoryState extends State<History> {
                       Navigator.pop(context);
                     });
                   },
-                )
+                ),
+        actions: isSelectionMode
+                  ? [
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        setState(() {
+                          for(int i = _selected.length-1; i >= 0 ; i--) {
+                            if(_selected[i]) _selectedDiet.meals.removeAt(i);
+                          }
+                          setSelection(false, _selectedDiet);
+                        });
+                      },
+                    ),
+                    Checkbox(
+                      value: totalCheck, 
+                      tristate: true,
+                      onChanged: (value) {
+                        setState(() {
+                          totalCheck = value ?? false;
+                          setSelection(value ?? false, _selectedDiet);
+                        });
+                      }
+                    ),
+                    const SizedBox(width: 24,)
+                  ] 
+                  : [const SizedBox.shrink()],
       ),
       
       body: ListView.builder(
-        itemCount: diet.meals.length,
+        itemCount: _selectedDiet.meals.length,
         itemBuilder:(context, index) => ListTile(
-          onTap: () {},
+          onTap: () {
+            if(isSelectionMode) {_toggle(index);}
+          },
+          onLongPress: () {
+            if (!isSelectionMode) {
+              setState(() {
+                isSelectionMode = true;
+                _selected[index] = true;
+              });
+            }
+          },
           title: Container(
             height: 50,
             decoration: BoxDecoration(
@@ -251,26 +361,122 @@ class _HistoryState extends State<History> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Spacer(),
-                Text(diet.meals[index].name),
+                Text(_selectedDiet.meals[index].name),
                 Spacer(),
-                Text(diet.meals[index].grams.toString()),
+                Text(_selectedDiet.meals[index].grams.toString()),
                 Spacer(),
-                Text(diet.meals[index].kcal.toString()),
+                Text(_selectedDiet.meals[index].kcal.toString()),
                 Spacer(),
               ],
             ),
           ),
+          trailing: isSelectionMode
+                  ? Checkbox(
+                      value: _selected[index],
+                      onChanged: (bool? x) {_toggle(index);},
+                    )
+                  : ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                    ),
+                    child: const Icon(Icons.add),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(builder: (context) => TodayMenu(
+                          diet : _selectedDiet,
+                          name : _selectedDiet.meals[index].name,
+                          gram : _selectedDiet.meals[index].grams,
+                          kcal : _selectedDiet.meals[index].kcal
+                        ))
+                      );
+                    },
+                  ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Calandar(diet: _selectedDiet))
+          );
+        },
+        tooltip: 'Home menu',
+        child: const Icon(Icons.calendar_month_outlined),
       ),
     );
   }
 }
 
-class Calandar extends StatelessWidget {
-  const Calandar({super.key});
+class Calandar extends StatefulWidget {
+  final Diet diet;
+
+  const Calandar({super.key, required this.diet});
+
+  @override
+  State<Calandar> createState() => _CalandarState();
+}
+
+class _CalandarState extends State<Calandar> {
+  DateTimeRange _selectedDateRange = DateTimeRange(
+    start: DateTime.now(), 
+    end: DateTime.now()
+  );
+
+  String showDate(DateTimeRange dateRange) {
+    return '${dateRange.start.year}.${dateRange.start.month}.${dateRange.start.day} ~ '
+      '${dateRange.end.year}.${dateRange.end.month}.${dateRange.end.day}'
+    ;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Calandar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Selected date range is : ${showDate(_selectedDateRange)}', style: const TextStyle(fontSize: 18)),
+            ElevatedButton(
+              onPressed: () async {
+                _selectedDateRange = await showDateRangePicker(
+                  context: context,
+                  currentDate: DateTime.now(),
+                  firstDate: DateTime(2021), 
+                  lastDate: DateTime.now()
+                ) ?? _selectedDateRange;
+                setState(() {});
+              },
+              child: const Text('choose'),
+            ),
+            const SizedBox(height: 80),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                fixedSize: const Size(300, 100),
+              ),
+              child: const Text('show diet in range', style: TextStyle(fontSize: 18)),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => History(diet: widget.diet, dateRange: _selectedDateRange,))
+                  );
+              },
+            ),
+          ]
+        ),
+      ),
+    );
   }
 }
--- lib/today.dart
+++ lib/today.dart
@@ -1,15 +1,36 @@
+import 'package:flutter/material.dart';
+
 class Meal {
   String name;
   int grams;
   int kcal;
+  DateTime time;
 
-  Meal(this.name, this.grams, this.kcal);
+  Meal(this.name, this.grams, this.kcal, {DateTime? date}) : time = date ?? DateTime.now();
 }
 
-class TodayDiet{
-  var kcal = 0;
-  var grams = 0;
-  var meals = <Meal>[];
+class Diet{
+  late int kcal;
+  late int grams;
+  late List<Meal> meals;
+
+  Diet({this.kcal = 0, this.grams = 0, List<Meal>? meals}) {
+    this.meals = meals ?? <Meal>[Meal('first', 100, 4, date: DateTime(2024, 1, 3)), Meal('second', 130, 20)];
+  }
+  
+  Diet dietOnDateRange(DateTimeRange dateRange) {
+    var newDiet = Diet(meals: []);
+    for (Meal m in meals) {
+      if(
+        m.time.isAtSameMomentAs(DateTime(dateRange.start.year, dateRange.start.month, dateRange.start.day)) || 
+        m.time.isAfter(DateTime(dateRange.start.year, dateRange.start.month, dateRange.start.day)) && 
+        m.time.isBefore(DateTime(dateRange.end.year, dateRange.end.month, dateRange.end.day+1))
+      ) {
+        newDiet.addInfo(m);
+      }
+    }
+    return newDiet;
+  }
 
   void addInfo(Meal food){
     meals.add(food);
