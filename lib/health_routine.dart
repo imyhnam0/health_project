bf5bbe75590f0735e211e7aa4959a148603debb4
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-01-07 22:53:40 +0900

    new 1.07

--- lib/health_routine.dart
+++ lib/health_routine.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Exercise {
+  String mainname;
   String name;
   int sets;
   double kg;
   int reps;
 
   Exercise({
+    required this.mainname,
     required this.name,
     required this.sets,
     required this.kg,
@@ -14,6 +16,8 @@ class Exercise {
 
   });
 }
+
+List<Exercise> exercises = [];
 
 class CreateRoutine extends StatefulWidget {
   const CreateRoutine({Key? key}) : super(key: key);
@@ -24,18 +28,16 @@ class CreateRoutine extends StatefulWidget {
 
 class _CreateRoutineState extends State<CreateRoutine> {
 
+  var mainnameController = TextEditingController();
   var nameController = [TextEditingController()];
   var setsController = [TextEditingController()];
   var kgController = [TextEditingController()];
   var repsController = [TextEditingController()];
 
-
-  List<Exercise> exercises = [];
   List<Widget> textFieldRows = [];
 
   @override
  void initState() {
-
     super.initState();
     textFieldRows.add( // Initial row of text fields
         Row(
@@ -45,54 +47,15 @@ class _CreateRoutineState extends State<CreateRoutine> {
                 width: 100,
                 height: 100,
                 child: TextField(
-                  controller: nameController[0],
+                  controller: mainnameController,
                   decoration: InputDecoration(
-                    labelText: 'Name: ',
+                    labelText: 'Routine Name: ',
                     border: OutlineInputBorder(),
                   ),
                 ),
               ),
             ),
-            Expanded(
-              child: Container(
-                width: 100,
-                height: 100,
-                child: TextField(
-                  controller: setsController[0],
-                  decoration: InputDecoration(
-                    labelText: 'Sets: ',
-                    border: OutlineInputBorder(),
-                  ),
 
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: 100,
-                height: 100,
-                child: TextField(
-                  controller: kgController[0],
-                  decoration: InputDecoration(
-                    labelText: 'kg: ',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: 100,
-                height: 100,
-                child: TextField(
-                  controller: repsController[0],
-                  decoration: InputDecoration(
-                    labelText: 'reps: ',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-            ),
           ],
         ));
   }
@@ -109,14 +72,6 @@ class _CreateRoutineState extends State<CreateRoutine> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
 
-
-                TextField(
-
-                  decoration: InputDecoration(
-                    labelText: 'Name: ',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
               const SizedBox(height: 10),
               // Display the list of text field rows
               Column(children: textFieldRows),
@@ -153,17 +108,22 @@ class _CreateRoutineState extends State<CreateRoutine> {
                            ),
                            onPressed: () {
                              Navigator.of(context).pop();
+                             Navigator.push(
+                                 context,
+                                 MaterialPageRoute(builder: (context) => const HealthRoutineMenu()));
 
                              setState(() {
                                for(int i=0;i<nameController.length;i++)
                                {
                                  Exercise exercise = Exercise(
+                                   mainname: mainnameController.text,
                                    name: nameController[i].text,
                                    sets: int.parse(setsController[i].text),
                                    kg: double.parse(kgController[i].text),
                                    reps: int.parse(repsController[i].text),
                                  );
                                  exercises.add(exercise);
+
 
                                }
 
@@ -254,7 +214,7 @@ class _CreateRoutineState extends State<CreateRoutine> {
                     width: 100,
                     height: 100,
                     child: TextField(
-                      controller: nameController[nameController.length-1],
+                      controller: nameController.isNotEmpty ? nameController[textFieldRows.length - 1] : nameController[0],
                       decoration: InputDecoration(
                         labelText: 'Name: ',
                         border: OutlineInputBorder(),
@@ -267,7 +227,7 @@ class _CreateRoutineState extends State<CreateRoutine> {
                     width: 100,
                     height: 100,
                     child: TextField(
-                      controller: setsController[setsController.length-1],
+                      controller: setsController.isNotEmpty ? setsController[textFieldRows.length - 1] : setsController[0],
                       decoration: InputDecoration(
                         labelText: 'Sets: ',
                         border: OutlineInputBorder(),
@@ -280,7 +240,7 @@ class _CreateRoutineState extends State<CreateRoutine> {
                     width: 100,
                     height: 100,
                     child: TextField(
-                      controller: kgController[kgController.length-1],
+                      controller: kgController.isNotEmpty ? kgController[textFieldRows.length - 1] : kgController[0],
                       decoration: InputDecoration(
                         labelText: 'kg: ',
                         border: OutlineInputBorder(),
@@ -293,7 +253,7 @@ class _CreateRoutineState extends State<CreateRoutine> {
                     width: 100,
                     height: 100,
                     child: TextField(
-                      controller: repsController[repsController.length-1],
+                      controller: repsController.isNotEmpty ? repsController[textFieldRows.length - 1] : repsController[0],
                       decoration: InputDecoration(
                         labelText: 'reps: ',
                         border: OutlineInputBorder(),
@@ -314,10 +274,101 @@ class _CreateRoutineState extends State<CreateRoutine> {
   }
 }
 
-class Favorite extends StatelessWidget{// 추가된 부분
-  const Favorite({super.key});// 생성자 수정
+
+class RoutineWidget extends StatelessWidget {
+  final String routineName;
+  final List<Exercise> exercises;
+
+  const RoutineWidget({Key? key, required this.routineName, required this.exercises})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ElevatedButton(
+        onPressed: () {
+          // 버튼이 눌렸을 때 수행할 동작 추가
+          // 예를 들어, 해당 루틴에 대한 상세 정보를 표시하는 다이얼로그를 띄울 수 있습니다.
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Routine Details'),
+                content: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Routine Name: $routineName'),
+                    for (Exercise exercise in exercises)
+                      Text(
+                        '  - Name: ${exercise.name}, Sets: ${exercise.sets}, KG: ${exercise.kg}, Reps: ${exercise.reps}',
+                      ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Close'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Text(routineName),
+      ),
+    );
+  }
+}
 
 
+
+class Favorite extends StatelessWidget {
+  final List<Exercise> exercises;
+
+  const Favorite({Key? key, required this.exercises}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 중복된 루틴 이름(mainname)을 제거한 리스트
+    List<String> uniqueRoutineNames =
+    exercises.map((exercise) => exercise.mainname).toSet().toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Exercises'),
+      ),
+      body: ListView.builder(
+        itemCount: uniqueRoutineNames.length,
+        itemBuilder: (context, index) {
+          String routineName = uniqueRoutineNames[index];
+          List<Exercise> routineExercises = exercises
+              .where((exercise) => exercise.mainname == routineName)
+              .toList();
+
+          return RoutineWidget(routineName: routineName, exercises: routineExercises);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        tooltip: 'Home menu',
+        child: Icon(Icons.home),
+      ),
+    );
+  }
+}
+
+class Calender extends StatefulWidget{
+  const Calender({super.key});
+
+  @override
+  State<Calender> createState() => _CalenderState();
+}
+
+class _CalenderState extends State<Calender> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -326,15 +377,19 @@ class Favorite extends StatelessWidget{// 추가된 부분
 
       // main page로 돌아가는 버튼
       // navigator 사용법은 다음 웹페이지에서 설명함
-
+      // https://docs.flutter.dev/cookbook/navigation/navigation-basics
       floatingActionButton: FloatingActionButton(
-        onPressed: () {Navigator.pop(context);},
+        onPressed: () {
+          Navigator.pop(context);
+        },
         tooltip: 'Home menu',
         child: const Icon(Icons.home),
       ),
     );
   }
 }
+
+
 
 class HealthRoutineMenu extends StatelessWidget{
   const HealthRoutineMenu({super.key});
@@ -385,8 +440,7 @@ class HealthRoutineMenu extends StatelessWidget{
                 onPressed: (){
                   Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const Favorite(),
-                      )
+                      MaterialPageRoute(builder: (context) => Favorite(exercises: exercises))
                   );
                 }
 
@@ -404,7 +458,12 @@ class HealthRoutineMenu extends StatelessWidget{
                 child: const Text('달력',
                   style: TextStyle(fontSize: 18),
                 ),
-                onPressed: (){}
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Calender()),
+                  );
+                }
 
             ),
           ],
